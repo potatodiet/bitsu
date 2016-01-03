8a552980117ed307150143752b83021634f46601
Justin Harrison <justin@matthin.com>
2016-01-03 06:15:44 -0500

    Store callback type in a typedef

--- src/main.cc
+++ src/main.cc
@@ -4,9 +4,10 @@
 #include <unordered_map>
 
 int main() {
-  std::unordered_map<std::string, std::function<void()>> routes;
-  routes["/book/new/"] = []() { std::cout << "test\n"; };
-  bitsu::Router<std::function<void()>> router(routes);
+  typedef std::function<void()> CallbackType;
+  const std::unordered_map<std::string, CallbackType> routes = {
+      {"/book/new/", []() { std::cout << "test\n"; }}};
+  bitsu::Router<CallbackType> router(routes);
   router.checkRoutes("/book/new/");
   return 0;
 }
